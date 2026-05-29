97fd0d6a965f818ba726807f3d7b8d0bf93edfa1
Rufus <dcrdevops24@gmail.com>
2026-05-29 10:55:08 +0530

    Update backend.tf

--- pillars/dev/backend.tf
+++ pillars/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "digi-dev-tf-s3-bucket"
+    bucket         = "dcr-dev-tf-s3-bucket"
     key            = "eks/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "digi-dev-tf-lock-state"
